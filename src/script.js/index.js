f2f9b94361a10c7b69b158e8a004d065fc2b5719
Gustavo <gucsantostec@gmail.com>
2022-08-26 10:26:46 -0300

    feat: add index.html,script.js

---
+++ src/script.js/index.js
@@ -0,0 +1,32 @@
+import { ApiRequest } from "./request.js";
+
+class PaginaLogin {
+    static renderizarPaginaLogin (){
+        const token = localStorage.getItem("@blogKenzie:token")
+
+        if(token) {
+            window.location.assign("src/pages/home.html")
+        }
+        const emailInput = document.getElementById("emailInput")
+        const passwordInput = document.getElementById("passwordInput")
+        const btnLogin = document.getElementById("btnLogin")
+
+        btnLogin.addEventListener("click" , (e) =>{
+            e.preventDefault()
+
+            const dados = {
+                email: emailInput.value,  
+                passwordInput : passwordInput.value
+            }
+            ApiRequest.login(dados)
+        })
+    }
+    static cadastreAqui() {
+        const cliqueAqui = document.getElementById("cliqueCadastro")
+        cliqueAqui.addEventListener("click", () =>{
+            window.location.assign("src/pages/home.html")
+        })
+    }
+}
+PaginaLogin.renderizarPaginaLogin()
+PaginaLogin.cadastreAqui()
---
+++ src/script.js/request.js
@@ -0,0 +1,23 @@
+export class ApiRequest {
+    static baseUrl = "https://blog-m2.herokuapp.com"
+    static token = localStorage.getItem("@blogKenzie:token") || ""
+    static headers = {
+        "Content-Type": application/json,
+        Authorization: `Bearer ${this.token}`
+    }
+    static async login (body){
+        const usuarioLogin = await fetch(`${this.baseUrl}/users/login`, {
+            method: "POST",
+            headers : this.headers,
+            body: json.stringfy(body)
+        })
+        .then(res => res.json)
+        .then(res => {
+            localStorage.setItem("@blogKenzie:token" , res.token)
+            localStorage.setItem("@blogKenzie:Usuario_Id", res.userId)
+        })
+        .catch(err => console.log(err))
+
+        return usuarioLogin
+    }
+}
